e4593944910c3ae5cfd688f8c75ca7e82cb92983
Ray Qiu <rayqiu@xage.com>
2024-07-30 14:19:32 -0500

    remove python for now

--- python_plugin.py
+++
@@ -1,8 +0,0 @@
-from jsonrpcserver import method, serve
-
-class GreeterPlugin:
-    @method
-    def greet(self, name):
-        return f"Hello {name} from the Python plugin GreeterPlugin."
-
-serve(GreeterPlugin(), address="localhost", port=8080)
